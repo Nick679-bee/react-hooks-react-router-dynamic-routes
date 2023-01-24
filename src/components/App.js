86f146c9ce3f7a95de5f0bdc45ce2430af42a5fb
nick <mwauranicholas10@gmail.com>
2023-01-24 12:12:58 +0300

    completed components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
+import MovieShow from "./MovieShow";
+import MoviesList from "./MovieList";
 
 function App() {
   const [movies, setMovies] = useState({
